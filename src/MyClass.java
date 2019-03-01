e853d75900807914a5016cbe3936340f193f94c1
NynrahGhost <xxyyzzghost@gmail.com>
2019-03-01 19:43:22 +0300

    Changed text in println

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,7 +3,7 @@ public class MyClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("I take it back. Change it.");
+		System.out.println("Changed it once more.");
 	}
 
 }
